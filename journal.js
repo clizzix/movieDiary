7ec4b3db9b800e049e316363306c0956187f7fac
clizzix <godsilla1993@gmail.com>
2025-12-06 13:06:59 +0100

    Added fetch logic as a Placeholder to design favorite cards

--- journal.js
+++ journal.js
@@ -9,15 +9,44 @@ const options = {
 };
 
 const container = document.getElementById('movie-cards');
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 
 const fetchPopularMovies = async () => {
     try {
         const res = await fetch(url, options);
         const data = await res.json();
-        console.log(data);
+        createMovieCards(data.results);
     } catch (e) {
         console.error(e);
     }
+};
+
+const createMovieCards = (movies) => {
+    movies.forEach((movie) => {
+        const movieCard = document.createElement('div');
+        movieCard.classList.add(
+            'bg-slate-950',
+            'm-8',
+            'p-8',
+            'text-white',
+            'rounded-xl',
+            'border-2',
+            'border-blue-800',
+            'shadow-lg',
+            'shadow-blue-800/50'
+        );
+        movieCard.innerHTML = `
+            <div class="flex gap-8 items-center">
+              <img class="w-48 h-auto object-cover flex-shrink-0" src=${imageBaseUrl}${movie.poster_path} alt="${movie.original_title} Poster" />
+            <div class="flex flex-1 justify-center">
+                <div class="flex flex-col gap-4 max-w-prose text-center">
+                    <h3 class="text-3xl">${movie.original_title}</h3>
+                    <p>${movie.overview}</p>
+                </div>
+            </div>
+            </div>`;
+        container.appendChild(movieCard);
+    });
 };
 
 fetchPopularMovies();
